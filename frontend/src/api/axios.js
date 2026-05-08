97ab27c37f366eb0e9c29d1435c57d78b470111a
Prince Chaudhary <princechaudhary6800@gmail.com>
2026-05-08 07:58:55 +0530

    updated api url for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://mern-ecommerce-app-np0p.onrender.com/api",
 });
 
 export default api;
